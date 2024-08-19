a8dfdfe7e3a58ae58b7ebbe5ed04ab7334e49004
Scribble-Sheep <15377810-AlyxisDrayko@users.noreply.gitlab.com>
2024-08-19 09:49:38 -0500

    Add fugitive, get plugins separated for vsc stuff

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -10,8 +10,21 @@
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  {
+    import = 'kickstart/plugins-nvim',
+    cond = function()
+      return not vim.g.vscode
+    end,
+  },
+  { import = 'kickstart/plugins-always', cond = true },
+  {
+    import = 'kickstart/plugins-vscode',
+    cond = function()
+      return vim.g.vscode
+    end,
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  --'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -19,30 +32,28 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
-
-
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
+  --require 'kickstart/plugins/gitsigns',
 
-  require 'kickstart/plugins/which-key',
+  --require 'kickstart/plugins/which-key',
 
-  require 'kickstart/plugins/telescope',
+  --require 'kickstart/plugins/telescope',
 
-  require 'kickstart/plugins/lspconfig',
+  --require 'kickstart/plugins/lspconfig',
 
-  require 'kickstart/plugins/conform',
+  --require 'kickstart/plugins/conform',
 
-  require 'kickstart/plugins/cmp',
+  --require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/nord',
+  --require 'kickstart/plugins/nord',
 
-  require 'kickstart/plugins/todo-comments',
+  --require 'kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
+  --require 'kickstart/plugins/mini',
 
-  require 'kickstart/plugins/treesitter',
+  --require 'kickstart/plugins/treesitter',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
